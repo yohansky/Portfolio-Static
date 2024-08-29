39a1278be4cfdf7d3bfb310a1520ce48d5f103e3
Yohanes <yohanesdahlan@gmail.com>
2024-08-29 11:42:55 +0700

    update project

--- src/pages/portofolios/index.js
+++ src/pages/portofolios/index.js
@@ -50,6 +50,7 @@ const Portfolios = ({ portfolios: initialPortfolios }) => {
               </PortfolioCard>
             </Col>
           ))} */}
+          {/* Blanja */}
           <Col
             // key={portfolio._id}
             md="4"
@@ -65,81 +66,81 @@ const Portfolios = ({ portfolios: initialPortfolios }) => {
             <Card className="portfolio-card">
               <CardHeader className="portfolio-card-header">Fullstack Web</CardHeader>
               <CardBody>
-                <p className="portfolio-card-city">Golang,React</p>
+                <p className="portfolio-card-city">Fiber(Golang), ReactJS, PostgreSQL</p>
                 <CardTitle className="portfolio-card-title">Blanja</CardTitle>
                 <CardText className="portfolio-card-text"></CardText>
-                {/* {children} */}
               </CardBody>
             </Card>
           </Col>
+          {/* Food Recipe */}
           <Col
             md="4"
             onClick={() => {
-              window.location.href = "https://github.com/yohansky/Fe-Blanja-React";
+              window.location.href = "https://github.com/yohansky/Fe-Food-Recipe";
             }}
             style={{ cursor: "pointer" }}
           >
             <Card className="portfolio-card">
-              <CardHeader className="portfolio-card-header">Fullstack ENgineer</CardHeader>
+              <CardHeader className="portfolio-card-header">Fullstack Web</CardHeader>
               <CardBody>
-                <p className="portfolio-card-city">Jakarta</p>
-                <CardTitle className="portfolio-card-title">Title</CardTitle>
-                <CardText className="portfolio-card-text">Ini test</CardText>
-                {/* {children} */}
+                <p className="portfolio-card-city">Fiber(Golang), ReactJS, PostgreSQL</p>
+                <CardTitle className="portfolio-card-title">Mama Recipe</CardTitle>
+                <CardText className="portfolio-card-text"></CardText>
               </CardBody>
             </Card>
-
-            {/* <>
-                <Button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    router.push("/portofolios/[id]/edit", `/portofolios/${portfolio._id}/edit`);
-                  }}
-                  className="mr-2"
-                  color="warning"
-                >
-                  Edit
-                </Button>
-                <Button onClick={(e) => _deletePortfolio(e, portfolio._id)} color="danger">
-                  Delete
-                </Button>
-              </> */}
-            {/* </PortfolioCard> */}
           </Col>
+          {/* Pe World */}
           <Col
             md="4"
             onClick={() => {
-              window.location.href = "https://github.com/yohansky/Fe-Blanja-React";
+              window.location.href = "https://github.com/yohansky/Fe-HireJob-Next";
             }}
             style={{ cursor: "pointer" }}
           >
             <Card className="portfolio-card">
-              <CardHeader className="portfolio-card-header">Fullstack ENgineer</CardHeader>
+              <CardHeader className="portfolio-card-header">Fullstack Web</CardHeader>
               <CardBody>
-                <p className="portfolio-card-city">Jakarta</p>
-                <CardTitle className="portfolio-card-title">Title</CardTitle>
-                <CardText className="portfolio-card-text">Ini test</CardText>
-                {/* {children} */}
+                <p className="portfolio-card-city">Fiber(Golang), NextJS, PostgreSQL</p>
+                <CardTitle className="portfolio-card-title">Peworld Hiring</CardTitle>
+                <CardText className="portfolio-card-text"></CardText>
               </CardBody>
             </Card>
-
-            {/* <>
-                <Button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    router.push("/portofolios/[id]/edit", `/portofolios/${portfolio._id}/edit`);
-                  }}
-                  className="mr-2"
-                  color="warning"
-                >
-                  Edit
-                </Button>
-                <Button onClick={(e) => _deletePortfolio(e, portfolio._id)} color="danger">
-                  Delete
-                </Button>
-              </> */}
-            {/* </PortfolioCard> */}
           </Col>
+          {/* Laravel FS */}
+          <Col
+            md="4"
+            onClick={() => {
+              window.location.href = "https://github.com/yohansky/Minicamp-Laravel1";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">Fullstack Web</CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city">Laravel(PHP), MySQL</p>
+                <CardTitle className="portfolio-card-title">E-Commerce Laravel</CardTitle>
+                <CardText className="portfolio-card-text"></CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          {/* Rest API Express Mongo */}
+          <Col
+            md="4"
+            onClick={() => {
+              window.location.href = "https://github.com/yohansky/express-basic";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">Backend</CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city">ExpressJS, MongoDB</p>
+                <CardTitle className="portfolio-card-title">Express REST API</CardTitle>
+                <CardText className="portfolio-card-text"></CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          {/*  */}
         </Row>
       </BasePage>
     </BaseLayout>
