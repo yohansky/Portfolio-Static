ed0d04e0fcea3cece48ab9972558fbf5212efaf4
Yohanes <yohanesdahlan@gmail.com>
2024-08-29 10:40:42 +0700

    first commit

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,12 +1,21 @@
 import { Card, CardHeader, CardBody, CardText, CardTitle } from "reactstrap";
 
 const PortfolioCard = ({ portfolio, children }) => (
+  // <Card className="portfolio-card">
+  //   <CardHeader className="portfolio-card-header">{portfolio.jobTitle}</CardHeader>
+  //   <CardBody>
+  //     <p className="portfolio-card-city">{portfolio.location}</p>
+  //     <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
+  //     <CardText className="portfolio-card-text">{portfolio.description}</CardText>
+  //     {children}
+  //   </CardBody>
+  // </Card>
   <Card className="portfolio-card">
-    <CardHeader className="portfolio-card-header">{portfolio.jobTitle}</CardHeader>
+    <CardHeader className="portfolio-card-header">Fullstack ENgineer</CardHeader>
     <CardBody>
-      <p className="portfolio-card-city">{portfolio.location}</p>
-      <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
-      <CardText className="portfolio-card-text">{portfolio.description}</CardText>
+      <p className="portfolio-card-city">Jakarta</p>
+      <CardTitle className="portfolio-card-title">Title</CardTitle>
+      <CardText className="portfolio-card-text">Ini test</CardText>
       {children}
     </CardBody>
   </Card>
--- src/pages/portofolios/index.js
+++ src/pages/portofolios/index.js
@@ -5,7 +5,7 @@ import PortfolioCard from "@/components/PortfolioCard";
 import PortfolioApi from "@/lib/api/portfolios";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { Row, Col, Button } from "reactstrap";
+import { Row, Col, Button, Card, CardHeader, CardBody, CardTitle, CardText } from "reactstrap";
 
 const Portfolios = ({ portfolios: initialPortfolios }) => {
   const router = useRouter();
@@ -23,7 +23,7 @@ const Portfolios = ({ portfolios: initialPortfolios }) => {
     <BaseLayout>
       <BasePage header="Portfolios" className="portfolio-page">
         <Row>
-          {portfolios.map((portfolio) => (
+          {/* {portfolios.map((portfolio) => (
             <Col
               key={portfolio._id}
               md="4"
@@ -49,20 +49,110 @@ const Portfolios = ({ portfolios: initialPortfolios }) => {
                 </>
               </PortfolioCard>
             </Col>
-          ))}
+          ))} */}
+          <Col
+            // key={portfolio._id}
+            md="4"
+            // onClick={() => {
+            //   router.push("/portofolios/[id]", `/portofolios/${portfolio._id}`);
+            // }}
+            onClick={() => {
+              window.location.href = "https://github.com/yohansky/Fe-Blanja-React";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            {/* <PortfolioCard /> */}
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">Fullstack Web</CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city">Golang,React</p>
+                <CardTitle className="portfolio-card-title">Blanja</CardTitle>
+                <CardText className="portfolio-card-text"></CardText>
+                {/* {children} */}
+              </CardBody>
+            </Card>
+          </Col>
+          <Col
+            md="4"
+            onClick={() => {
+              window.location.href = "https://github.com/yohansky/Fe-Blanja-React";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">Fullstack ENgineer</CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city">Jakarta</p>
+                <CardTitle className="portfolio-card-title">Title</CardTitle>
+                <CardText className="portfolio-card-text">Ini test</CardText>
+                {/* {children} */}
+              </CardBody>
+            </Card>
+
+            {/* <>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    router.push("/portofolios/[id]/edit", `/portofolios/${portfolio._id}/edit`);
+                  }}
+                  className="mr-2"
+                  color="warning"
+                >
+                  Edit
+                </Button>
+                <Button onClick={(e) => _deletePortfolio(e, portfolio._id)} color="danger">
+                  Delete
+                </Button>
+              </> */}
+            {/* </PortfolioCard> */}
+          </Col>
+          <Col
+            md="4"
+            onClick={() => {
+              window.location.href = "https://github.com/yohansky/Fe-Blanja-React";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">Fullstack ENgineer</CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city">Jakarta</p>
+                <CardTitle className="portfolio-card-title">Title</CardTitle>
+                <CardText className="portfolio-card-text">Ini test</CardText>
+                {/* {children} */}
+              </CardBody>
+            </Card>
+
+            {/* <>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    router.push("/portofolios/[id]/edit", `/portofolios/${portfolio._id}/edit`);
+                  }}
+                  className="mr-2"
+                  color="warning"
+                >
+                  Edit
+                </Button>
+                <Button onClick={(e) => _deletePortfolio(e, portfolio._id)} color="danger">
+                  Delete
+                </Button>
+              </> */}
+            {/* </PortfolioCard> */}
+          </Col>
         </Row>
       </BasePage>
     </BaseLayout>
   );
 };
 
-export async function getStaticProps() {
-  const json = await new PortfolioApi().getAll();
-  const portfolios = json.data;
+// export async function getStaticProps() {
+//   const json = await new PortfolioApi().getAll();
+//   const portfolios = json.data;
 
-  return {
-    props: { portfolios },
-  };
-}
+//   return {
+//     props: { portfolios },
+//   };
+// }
 
 export default Portfolios;
